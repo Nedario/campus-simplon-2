9d63a48bce6c4a94086699aeb0bb63834d8617fe
owlab-io <contact@owlab.io>
2018-04-24 10:49:56 +0200

    ajout commentaire sur event-bus

--- 03_js/05_vuejs/43_node_backend/src/event-bus.js
+++ 03_js/05_vuejs/43_node_backend/src/event-bus.js
@@ -1,3 +1,15 @@
 /* jslint esversion:6 */
+
+// event bus permet de faire communiquer des composants directement, sans passer par les liens parents/enfants
+// il est disponible dans tous les composants de l'App.
+
+// pour émettre un event custom =>
+// this.$ebus.emit("custom-event-name", [value]);
+
+// pour réagir à un event-custom =>
+// this.$ebus.on("custom-event-name", (value) => {
+  // faire quelque chose avec value ici ...
+// });
+
 import Vue from 'vue';
 export const EventBus = new Vue();
