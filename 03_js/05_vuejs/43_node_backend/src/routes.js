fcf8949fc16df53f7652c0928a281fdc1d0bd375
owlab-io <contact@owlab.io>
2018-04-24 10:37:49 +0200

    update du routeur (beforeEach dashboard path)

--- 03_js/05_vuejs/43_node_backend/src/routes.js
+++ 03_js/05_vuejs/43_node_backend/src/routes.js
@@ -5,9 +5,14 @@
 import Vue from 'vue';
 import store from "./store/store";
 import VueRouter from "vue-router";
+
 import Home from "./components/pages/Home.vue";
 import Login from "./components/pages/Login.vue";
 import Dashboard from "./components/pages/Dashboard.vue";
+import DashboardStatics from "./components/dashboard-elements/DashboardStatics.vue";
+import DashboardUsers from "./components/dashboard-elements/DashboardUsers.vue";
+import ProductsDashboard from "./components/products/Dashboard.vue";
+import ProductsForm from "./components/products/Form.vue";
 import Todo from "./components/pages/Todo.vue";
 
 Vue.use(VueRouter);
@@ -40,7 +45,29 @@ const routes = [
       } else {
         next({ name: "login"});
       }
-    }
+    },
+    children: [
+      {
+        component: DashboardStatics,
+        name: "statistics",
+        path: "statistics",
+      },
+      {
+        component: DashboardUsers,
+        name: "manage-users",
+        path: "manage-users",
+      },
+      {
+        component: ProductsDashboard,
+        name: "manage-products",
+        path: "manage-products",
+      },
+      {
+        component: ProductsForm,
+        name: "add-product",
+        path: "add-product",
+      }
+    ]
   },
   {
     component: Todo,
